c9faaa27e82840a50195668635704a3bd4ca8757
Utkarsh Agarwal <61263193+Utkarsh-Agarwal-585@users.noreply.github.com>
2021-01-12 22:15:25 +0530

    Add files via upload
    Impact of Covoid-19 on IT Sector.

---
+++ Redesign the Website/Covoid-19 Analysis/Tech.php
@@ -0,0 +1,88 @@
+<?php
+
+$tech = implode($_POST["a"]);
+echo $tech;
+if($tech=="Artificial intelligence")
+{
+echo '<script> 
+    document.location="https://towardsdatascience.com/artificial-intelligence-against-covid-19-an-early-review-92a8360edaba";
+    </script>';
+}
+else if($tech=="Machine learning")
+{
+echo '<script>
+      document.location="https://www.datarevenue.com/en-blog/machine-learning-covid-19";
+       </script>';
+        }
+else if($tech=="IOT")
+{
+echo '<script>
+      document.location="https://iot-analytics.com/the-impact-of-covid-19-on-the-internet-of-things/";
+       </script>';
+        }
+else if($tech=="Deep learning")
+{
+echo '<script>
+      document.location="https://healthitanalytics.com/news/machine-learning-tools-predict-impact-of-quarantine-on-covid-19";
+       </script>';
+        }
+else if($tech=="Cyber security")
+{
+echo '<script>
+      document.location="https://healthitanalytics.com/news/machine-learning-tools-predict-impact-of-quarantine-on-covid-19";
+       </script>';
+        }
+else if($tech=="5G")
+{
+echo '<script>
+      document.location="https://www.marketsandmarkets.com/Market-Reports/covid-19-impact-on-5g-infrastructure-market-165448475.html";
+       </script>';
+        }
+else if($tech=="Automation")
+{
+echo '<script>
+      document.location="https://www.globenewswire.com/news-release/2020/05/12/2031882/0/en/Automation-Industry-Amid-Global-COVID-19-Coronavirus-Crisis-Meticulous-Research-Viewpoint.html";
+       </script>';
+        }
+else if($tech=="Analytics")
+{
+echo '<script>
+      document.location="https://www.globenewswire.com/news-release/2020/05/12/2031882/0/en/Automation-Industry-Amid-Global-COVID-19-Coronavirus-Crisis-Meticulous-Research-Viewpoint.html";
+       </script>';
+        }
+else if($tech=="Data scinece")
+{
+echo '<script>
+      document.location="https://inside.uncc.edu/news-features/2020-04-22/impact-covid-19-data-science-health-care";
+       </script>';
+        }
+
+else if($tech=="Quantum computing")
+{
+echo '<script>
+      document.location="https://www.globenewswire.com/news-release/2020/04/17/2017852/0/en/Quantum-Technologies-Market-Study-2020-Use-of-Supercomputers-to-Study-COVID-19-Impact-Creates-Potential-Applications-of-Quantum-Computing.html";
+       </script>';
+        }
+
+else if($tech=="3D printing")
+{
+echo '<script>
+      document.location="https://futurumresearch.com/futurum-tech-podcast/the-impact-3d-printing-is-having-on-coronavirus-covid-19-and-whats-ahead-futurum-tech-podcast/";
+       </script>';
+        }
+
+else if($tech=="Edge computing")
+{
+echo '<script>
+      document.location="https://www.amcoenclosures.com/sales-reps/";
+       </script>';
+        }
+else if($tech=="Block chain")
+{
+echo '<script>
+      document.location="https://economictimes.indiatimes.com/tech/internet/blockchain-technology-can-tackle-supply-chain-failures-exposed-by-covid-19-boost-economic-recovery-wef/articleshow/75430872.cms?from=mdr";
+       </script>';
+        }
+
+        
+?>
